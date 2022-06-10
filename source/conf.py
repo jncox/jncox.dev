c1749761a4c774007080f6620ca25c880de44d9f
Nathan Cox <jncox@users.noreply.github.com>
2022-06-10 10:26:57 -0700

    Another Try

--- source/conf.py
+++ source/conf.py
@@ -22,17 +22,19 @@ import sphinx_fontawesome
 # -- Project information -----------------------------------------------------
 
 project = 'HOLs'
-copyright = '2022, JNCOX'
+copyright = '2022, jncox'
 author = 'jncox'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
+
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = ['sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinxcontrib.fulltoc',
@@ -41,7 +43,6 @@ extensions = ['sphinx.ext.todo',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -52,10 +53,10 @@ source_suffix = ['.rst', '.md']
 # The master toctree document.
 master_doc = 'index'
 
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+#exclude_patterns = []
 
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
@@ -65,7 +66,6 @@ latex_elements = {
 }
 
 latex_toplevel_sectioning = 'section'
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -80,7 +80,6 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-
 html_static_path = ['_static']
 
 html_logo = "_static/vectra.png"
