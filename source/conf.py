78a362444545d36d992c964d0e451c32c1f5fe07
Nathan Cox <jncox@users.noreply.github.com>
2022-06-10 17:26:41 -0700

    Updates for look and feel

--- source/conf.py
+++ source/conf.py
@@ -88,8 +88,8 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "_static/vectra.png"
-
+#html_logo = "_static/vectra.png"
+html_logo = "_static/vectra_logo.svg"
 html_favicon = "_static/favicon.ico"
 
 #html_css_files = [
