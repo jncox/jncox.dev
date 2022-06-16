91c3e8a018878674c8cbd5ee65540eda031745cb
Nathan Cox <jncox@users.noreply.github.com>
2022-06-16 09:41:32 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,6 @@
 import os
 import sys
 import sphinx_bootstrap_theme
-import sphinxawesome-theme
 import sphinx_fontawesome
 
 # -- Project information -----------------------------------------------------
